8dedbe11fab875b34567154b915fc467cfe50095
ivan-moskvin-data <ivan.data.mail@gmail.com>
2026-03-03 11:18:13 +0200

    perf: optimize memory usage with row-by-row iteration

--- 01_python_core/data_processing/csv_condenser/condenser.py
+++ 01_python_core/data_processing/csv_condenser/condenser.py
@@ -1,18 +1,41 @@
 import csv
 
-def condense_csv(filename, id_name="XXX"):
-    with open(filename, encoding="utf-8") as sf, open ("condensed.csv", "w", encoding="utf-8") as ef:
-        l = list(csv.reader(sf))
-        colunms = [id_name]
-        for c in l:
-            if c[1] not in colunms:
-                colunms.append(c[1])
-        nl = []
-        for i in range(0, len(l), len(colunms) - 1):
-            d = {id_name:l[i][0]}
-            for j in range(len(colunms) - 1):
-                d[l[i + j][1]] = l[i + j][2]
-            nl.append(d)
-        write = csv.DictWriter(ef, fieldnames=colunms)
-        write.writeheader()
-        write.writerows(nl)
+def get_unique_headers(filename):
+    columns_seen = {}
+    with open(filename, mode="r", encoding="utf-8") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if len(row) == 3:
+                columns_seen[row[1]] = None
+    return list(columns_seen.keys())
+
+def condense_csv(filename, id_name="ID"):
+    dynamic_headers = get_unique_headers(filename)
+    fieldnames = [id_name] + dynamic_headers
+
+    with open(filename, mode="r", encoding="utf-8") as sf, \
+         open("condensed.csv", mode="w", encoding="utf-8", newline="") as ef:
+        
+        reader = csv.reader(sf)
+        writer = csv.DictWriter(ef, fieldnames=fieldnames)
+        writer.writeheader()
+
+        current_id = None
+        row_buffer = {}
+
+        for row in reader:
+            if len(row) != 3:
+                continue
+            
+            obj_id, attr, value = row
+
+            if obj_id != current_id:
+                if current_id is not None:
+                    writer.writerow(row_buffer)
+                current_id = obj_id
+                row_buffer = {id_name: obj_id}
+            
+            row_buffer[attr] = value
+
+        if row_buffer:
+            writer.writerow(row_buffer)
